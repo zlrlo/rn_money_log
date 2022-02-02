50688b9373bcf3fb81981f9545fdd141c662e418
zlrlo <easy10612@gmail.com>
2022-02-02 18:06:20 +0900

    ✨ : GroupDetailImageCard 컴포넌트 적용
    - ScrollView 적용

--- lib/feature-group-detail/FeatureGroupDetail.tsx
+++ lib/feature-group-detail/FeatureGroupDetail.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {ScrollView, Text, View} from 'react-native';
 import tw from 'twrnc';
 import {GroupDetailCountBoxes} from './GroupDetailCountBoxes';
 import {GroupDetailHeader} from './GroupDetailHeader';
+import {GroupDetailImageCard} from './GroupDetailImageCard';
 import {GroupDetailProfile} from './GroupDetailProfile';
 
 export const FeatureGroupDetail = () => {
   return (
-    <View style={tw`px-4`}>
-      <GroupDetailHeader />
-      <GroupDetailProfile />
-      <View style={tw`my-9`}>
-        <GroupDetailCountBoxes />
+    <View>
+      <View style={tw`px-4`}>
+        <GroupDetailHeader />
       </View>
-      {/* image card slider */}
-      <View style={tw`border-2`}>
-        <Text>image card slider</Text>
-      </View>
+      <ScrollView style={tw`px-4`}>
+        <GroupDetailProfile />
+        <View style={tw`my-9`}>
+          <GroupDetailCountBoxes />
+        </View>
+        <GroupDetailImageCard />
+      </ScrollView>
     </View>
   );
 };
