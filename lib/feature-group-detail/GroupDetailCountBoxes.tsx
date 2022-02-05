569212c598cd30666cc79915b35dce276f668aea
zlrlo <easy10612@gmail.com>
2022-02-05 18:24:06 +0900

    💄 : GroupDetailCountBoxes 컴포넌트 스타일 수정

--- lib/feature-group-detail/GroupDetailCountBoxes.tsx
+++ lib/feature-group-detail/GroupDetailCountBoxes.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 import tw from 'twrnc';
 
 export const GroupDetailCountBoxes = () => {
   return (
-    <View style={tw`border-2 flex-row justify-between`}>
-      <View style={tw`border-2 w-14 h-14`}></View>
-      <View style={tw`border-2 w-14 h-14`}></View>
-      <View style={tw`border-2 w-14 h-14`}></View>
-      <View style={tw`border-2 w-14 h-14`}></View>
+    <View style={tw`border-b-2 flex-row justify-between px-5`}>
+      <View
+        style={tw`w-16 h-14 p-1 items-center border-t-2 border-l-2 border-r-2`}>
+        <Text style={tw`text-xs`}>2022년</Text>
+        <Text style={tw`text-base font-bold flex-1`}>2월</Text>
+      </View>
+      <View style={tw`w-16 h-14 p-1 items-center`}>
+        <Text style={tw`text-xs`}>총 소비</Text>
+        <Text style={tw`text-base font-bold flex-1`}>32만</Text>
+      </View>
+      <View style={tw`w-16 h-14 p-1 items-center`}>
+        <Text style={tw`text-xs`}>최고 소비</Text>
+        <Text style={tw`text-base font-bold flex-1`}>5만</Text>
+      </View>
+      <View style={tw`w-16 h-14 p-1 items-center`}>
+        <Text style={tw`text-xs`}>최저 소비</Text>
+        <Text style={tw`text-base font-bold flex-1`}>0.5만</Text>
+      </View>
     </View>
   );
 };
